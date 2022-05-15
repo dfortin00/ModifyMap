24e5ea85af2fdf74f92761f30e5eb9abe447ef9a
dfortin00 <quadratonx@gmail.com>
2022-05-15 16:22:51 -0400

    Comment setup batch file

--- test/setup_test_lib.bat
+++ test/setup_test_lib.bat
@@ -1,5 +1,8 @@
 rmdir /Q /S lib\mapmatic
 rmdir /Q /S lib\mapmatic
 
+rem Modify source directory to the location of the MapMatic library before executing.
 xcopy ..\..\..\MapMatic\mapmatic  lib\mapmatic /E/H/C/I
+
+rem Copies the ModifyMap folder into the mapmatic/plugins folder for testing purposes.
 xcopy ..\ModifyMap  lib\mapmatic\plugins\ModifyMap /E/H/C/I
